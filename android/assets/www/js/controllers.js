caecc2b6fc90221072051c01fc9d525ce9506de1
Edwin Hernández <edinfamous@gmail.com>
2016-03-09 07:50:17 -0500

    Ajustes de textos

--- android/assets/www/js/controllers.js
+++ android/assets/www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $state,
-  $ionicPopup, $rootScope, $ionicLoading){
+  $ionicPopup, $rootScope, $ionicLoading, $cordovaSocialSharing){
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -43,6 +43,74 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
   $scope.$watch('data.rating', function() {
     console.log('New value: '+$scope.data.rating);
   });
+
+  //Social Share
+  $cordovaSocialSharing
+    .share(message, subject, file, link) // Share via native share sheet
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .shareViaTwitter(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .shareViaWhatsApp(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .shareViaFacebook(message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  // access multiple numbers in a string like: '0612345678,0687654321'
+  $cordovaSocialSharing
+    .shareViaSMS(message, number)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+// toArr, ccArr and bccArr must be an array, file can be either null, string or array
+  $cordovaSocialSharing
+    .shareViaEmail(message, subject, toArr, ccArr, bccArr, file)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .canShareVia(socialType, message, image, link)
+    .then(function(result) {
+      // Success!
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  $cordovaSocialSharing
+    .canShareViaEmail()
+    .then(function(result) {
+      // Yes we can
+    }, function(err) {
+      // Nope
+    });
+
 
   // Triggered in the message modal to close it
   $scope.closeMessage = function() {
